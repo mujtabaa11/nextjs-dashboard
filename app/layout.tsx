d2b3cb2c4ab031d998affc686ee954369e8d807d
mujtabaa11 <Mohamed.Mugtaba11@gmail.com>
2025-02-12 08:40:35 -0500

    Completed starter project.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: { template: '%s | Acme Dashboard' },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
+
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 
